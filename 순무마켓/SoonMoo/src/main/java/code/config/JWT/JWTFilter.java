2916fae510a5c44307e10e720312226dfcec597d
LYST <gabury1@gmail.com>
2024-08-01 00:45:50 +0900

    JWT 기본 적용 (Access Token + Refresh Token 구현X)

--- 순무마켓/SoonMoo/src/main/java/code/config/JWT/JWTFilter.java
+++ 순무마켓/SoonMoo/src/main/java/code/config/JWT/JWTFilter.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import code.domain.User.UserEntity;
 import code.dto.UserDto;
 import io.jsonwebtoken.io.IOException;
 import jakarta.servlet.FilterChain;
@@ -20,53 +21,56 @@ public class JWTFilter extends OncePerRequestFilter
 {
     private final JWTUtil jwtUtil;
 
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException, java.io.IOException {
-        //cookie들을 불러온 뒤 Authorization Key에 담긴 쿠키를 찾음
-        String authorization = null;
-        Cookie[] cookies = request.getCookies();
-        for (Cookie cookie : cookies) {
+        
+        log.info("진입");
+        // Cookie c = new Cookie("Authorization", null);
+        // c.setMaxAge(0);
+        // response.addCookie(c);
 
-            if (cookie.getName().equals("Authorization")) {
+ 		//request에서 Authorization 헤더를 찾음
+        String authorization= request.getHeader("Authorization");
+				
+		//Authorization 헤더 검증
+        if (authorization == null || !authorization.startsWith("Bearer")) {
 
-                authorization = cookie.getValue();
-            }
-        }
-
-        //Authorization 헤더 검증
-        if (authorization == null) {
-
-            log.warn("JWTFilter :::::: doFilterInternal :::::: token null");
+            System.out.println("token null : " + authorization);
             filterChain.doFilter(request, response);
-
-            //조건이 해당되면 메소드 종료 (필수)
+						
+						//조건이 해당되면 메소드 종료 (필수)
             return;
         }
-
-        //토큰
-        String token = authorization;
-
-        //토큰 소멸 시간 검증
+			
+        System.out.println("authorization now");
+				//Bearer 부분 제거 후 순수 토큰만 획득
+        String token = authorization.split("???")[1];
+			
+				//토큰 소멸 시간 검증
         if (jwtUtil.isExpired(token)) {
 
-            log.info("JWTFilter :::::: doFilterInternal :::::: token expired");
+            System.out.println("token expired");
             filterChain.doFilter(request, response);
 
-            //조건이 해당되면 메소드 종료 (필수)
+						//조건이 해당되면 메소드 종료 (필수)
             return;
         }
 
+				//토큰에서 username과 role 획득
+        String username = jwtUtil.getName(token);
+        String role = jwtUtil.getRole(token);
+				
+				//userEntity를 생성하여 값 set
+        UserDto dto = new UserDto();
+        dto.setUserName(username);
+        //dto.setPassword("text");
+        dto.setRole(role);
+	
 
-        //UserDetails에 회원 정보 객체 담기
-        var dto = jwtUtil.tokenToDto(token);
-
-        //스프링 시큐리티 인증 토큰 생성
+		//스프링 시큐리티 인증 토큰 생성
         Authentication authToken = new UsernamePasswordAuthenticationToken(dto, null, dto.getAuthorities());
-        //UsernamePasswordAuthenticationToken : 해당 유저 정보를 토큰에 담아줌.
-
-        //세션에 사용자 등록
+		//세션에 사용자 등록
         SecurityContextHolder.getContext().setAuthentication(authToken);
 
         filterChain.doFilter(request, response);
--- 순무마켓/SoonMoo/src/main/java/code/config/SecurityConfig.java
+++ 순무마켓/SoonMoo/src/main/java/code/config/SecurityConfig.java
@@ -3,36 +3,62 @@ package code.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import code.config.JWT.JWTFilter;
+import code.config.JWT.JWTProvider;
 import code.config.JWT.JWTUtil;
+import code.config.JWT.LoginFilter;
 import code.service.UserService;
+import groovy.util.logging.Log;
 
 @Configuration
 @EnableWebSecurity 
 public class SecurityConfig {
 
     private final UserService userService;
+    private final AuthenticationConfiguration authenticationConfiguration;
     private final JWTUtil jwtUtil;
+    private final JWTProvider jwtProvider;
 
-        public SecurityConfig(@Lazy UserService u, JWTUtil j)
-        {
-                userService = u;
-                jwtUtil = j;
-        }
+    public SecurityConfig(@Lazy UserService u, JWTUtil j, AuthenticationConfiguration a, JWTProvider p)
+    {
+        jwtUtil = j;
+        authenticationConfiguration = a;
+        userService = u;
+        jwtProvider = p;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         //csrf disable
         http
                 .csrf((auth) -> auth.disable());
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
 
         http
                 .formLogin(f -> f
@@ -40,17 +66,9 @@ public class SecurityConfig {
                                 .loginProcessingUrl("/login/loginProcess")
                                 .usernameParameter("userId")
                                 .passwordParameter("password")
-                                .defaultSuccessUrl("/")
+                                //.defaultSuccessUrl("/")
+                                .successHandler(jwtProvider)
                 );
-
-
-        //HTTP Basic 인증 방식 disable
-        http
-                .httpBasic((auth) -> auth.disable());
-
-        //JWTFilter 추가
-         http
-                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         // oauth2
 	http
@@ -61,7 +79,10 @@ public class SecurityConfig {
                         //.successHandler(customSeccessHandler)   
                         
                 );
-
+        
+        //JWTFilter 등록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         //경로별 인가 작업
         http
@@ -78,9 +99,5 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
 }
